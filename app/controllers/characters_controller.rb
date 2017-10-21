cdae13f1495ff18655c0354f80b06dec9ba4ad0a
Jennifer Nelson <jennifernelson118@gmail.com>
2017-10-21 18:55:05 +0000

    Can create new character and associate with traits

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,11 +1,27 @@
 class CharactersController < ApplicationController
 
   get '/characters/new' do
+    login_check
     erb :'characters/new'
   end
 
   post '/characters' do
-
+    binding.pry
+    # if !params[:character][:name] == Character.find_by(name: params[:character][:name])
+    #   @character = Character.new(params[:character])
+    #   @character.user_id = session[:user_id]
+    #   @character.trait_ids = params[:trait_ids]
+    #   if !params[:traits].empty?
+    #     params[:traits].each do |trait|
+    #       if trait != ""
+    #         @character.traits << Trait.create(name: trait)
+    #       end
+    #     end
+    #   end
+    #   @character.save
+    # else
+    #   flash[:message] = "You have already added this character. Consider editing instead."
+    # end
   end
 
   get '/characters' do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,22 +11,40 @@ class UsersController < ApplicationController
     @user = User.find_by(username: params[:username])
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
-    else
-      "Error" #make flash message
+      redirect '/characters'
     end
-    redirect '/characters'
+      flash[:message] = "We could not find an account associated with this username.  Try again or create an account."
+      redirect '/login'
   end
 
   get '/signup' do
+    if logged_in?
+      redirect '/characters'
+    end
     erb :'/users/signup'
   end
 
   post '/signup' do
-
+    if User.find_by(username: params[:user][:username])
+      flash[:message] = "There is already an account associated with this username. Try logging in."
+      redirect '/'
+    end
+    @user = User.new(params[:user])
+    @user.password_confirmation = params[:confirm_password]
+    if @user.valid?
+      @user.save
+      session[:user_id] = @user.id
+      redirect '/characters'
+    else
+      flash[:message] = "Your username and password do not match."
+      redirect '/signup'
+    end
   end
 
   get '/logout' do
-
+    login_check
+    session.clear
+    redirect '/'
   end
 
 end
